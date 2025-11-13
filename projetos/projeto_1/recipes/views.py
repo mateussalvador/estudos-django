25f63e277fb306af745a0f7792e7ac2ed63413ac
Mateus Salvador de Lima <E008803@rede.tst>
2025-11-13 16:41:35 -0300

    corrigindo erros e iniciando o css do projeto

--- projetos/projeto_1/recipes/views.py
+++ projetos/projeto_1/recipes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={"nome": "Mateus Salvador"})
+    return render(request, 'recipes/pages/home.html', context={"name": "Salvador"})
 
