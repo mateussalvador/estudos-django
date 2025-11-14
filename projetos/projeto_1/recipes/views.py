c7e55d79867a926b64c27596a556ac55e04853af
Mateus Salvador de Lima <E008803@rede.tst>
2025-11-14 16:42:26 -0300

    prosseguindo com templates

--- projetos/projeto_1/recipes/views.py
+++ projetos/projeto_1/recipes/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'recipes/pages/home.html', context={"name": "Salvador"})
 
+def recipes(request, id):
+    return render(request, 'recipes/pages/recipes_view.html')
